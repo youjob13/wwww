c5f8012d74cf85cdcb7f7dea821fdbafdc93e3b3
Danil Rodionov <Danil_Rodionov@epam.com>
2023-09-24 20:44:19 +0400

    2

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -1,12 +1,23 @@
-import { inject, Injectable } from "@angular/core";
-import { Translation, TranslocoLoader } from "@ngneat/transloco";
-import { HttpClient } from "@angular/common/http";
+import { inject, Injectable, isDevMode } from '@angular/core';
+import { Translation, TranslocoLoader } from '@ngneat/transloco';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
-    private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
-    getTranslation(lang: string) {
-        return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
-    }
+  getTranslation(lang: string) {
+    return fetch(
+      `${
+        !isDevMode() ? 'https://youjob13.github.io/wwww/docs' : ''
+      }/assets/i18n/${lang}.json`
+    ).then<Translation>((res) => res.json());
+    // return this.http.get<any>(`/assets/i18n/${lang}.json`).pipe(
+    //   map((res) => {
+    //     console.log('res', res);
+    //     return res;
+    //   })
+    // );
+  }
 }
