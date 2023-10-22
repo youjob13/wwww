8563e136bfe49fafeb93eec84c26a79565284fd7
Danil Rodionov <Danil_Rodionov@epam.com>
2023-10-22 17:33:55 +0400

    feat sth

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule, DOCUMENT } from '@angular/common';
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ElementRef,
@@ -8,7 +7,6 @@ import {
   inject,
 } from '@angular/core';
 import { LanguageTranslateService } from 'src/app/common/language-translate.service';
-import { calcScrollPercentage } from 'src/app/common/utils';
 
 @Component({
   selector: 'chirk-header',
@@ -18,49 +16,23 @@ import { calcScrollPercentage } from 'src/app/common/utils';
   standalone: true,
   imports: [CommonModule],
 })
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent {
   @ViewChild('hiddenToggler', { static: true }) hiddenToggler:
     | ElementRef
     | undefined;
 
   public readonly languageTranslateService = inject(LanguageTranslateService);
   private readonly document = inject(DOCUMENT);
-  private wrapper: Element | null = null;
-
-  ngAfterViewInit(): void {
-    this.wrapper = this.document.querySelector('.inner-body');
-    this.wrapper?.addEventListener('scroll', this.hideMenuOnScroll.bind(this));
-  }
 
   public changeLanguage(): void {
     this.languageTranslateService.changeLanguage();
   }
 
-  private hideMenuOnScroll(): void {
-    if (this.wrapper == null) {
-      return;
-    }
-
-    const scrollPercentage = calcScrollPercentage(this.wrapper, this.document);
-
-    if (
-      scrollPercentage > 0.02 &&
-      this.hiddenToggler?.nativeElement.checked === true
-    ) {
-      this.hideMenu();
-    }
-  }
-
   public scrollToSection(target: string) {
-    this.hideMenu();
     this.document.getElementById(target)?.scrollIntoView({
       behavior: 'smooth',
       block: 'start',
       inline: 'nearest',
     });
-  }
-
-  private hideMenu(): void {
-    this.hiddenToggler?.nativeElement.click();
   }
 }
